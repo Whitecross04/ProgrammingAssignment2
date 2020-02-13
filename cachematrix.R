36939e133ea3afff292396cd6cae391b72c89579
Whitecross04 <Whitecross04@users.noreply.github.com>
2020-02-13 10:47:34 +0100

    Cousera Assignment.
    Caching the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix is a function made to take in a matrix object
+## Its also a list that contains functions to set the vlue of th matrix,
+## get the value of the matrix, set the inverse of the matrix and also
+## get the inverse of the matrix.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        p <- NULL
+        set <- function(y){
+                x <<- y
+                p <<- NULL
+        }
+        get <- function()x
+        setInverse <- function(inverse) p <<- inverse
+        getInverse <- function() p 
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve calculates the inverese of the matrix which was 
+## created with the makeCacheMatrix function above.
+##it first checks to see if the inverse has  been obtained. If so, 
+## it gets the inverse from the cache and skips the computation. Otherwise, 
+## it calculates the inverse of the data and sets the inverse in 
+## the cache via the setInverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## This function will Return a matrix wich is an inverse of 'x'
+        p <- x$getInverse()
+        if(!is.null(p)){
+                message("obtaining cached data")
+                return(p)
+        }
+        
+        mat <- x$get()
+        p <- solve(mat,...)
+        x$setInverse(p)
+        p
 }
+
+#Testing the functions
+Mattt<- matrix(rnorm(9, mean = 3, sd = 1.25), 3,3)
+Amatrix<- makeCacheMatrix(Mattt)
+cacheSolve(Amatrix)
